20ef5701328eb0918512e0e038d1d67878b6ebdf
ROMANGREKHOV <94071555+ROMANGREKHOV@users.noreply.github.com>
2026-01-04 12:42:18 +0300

    DZ11_1.cpp

--- DZ11_1.cpp
+++ DZ11_1.cpp
@@ -1,22 +1,23 @@
-//#include <iostream>
-//
-//int main()
-//{
-//	int size{}, cnt{};
-//	std::cout << "Введите размер массива: ";
-//	std::cin >> size;
-//	int* arr = new int[size]();
-//	for (int i = 0; i < size; ++i)
-//	{
-//		std::cout << "arr[" << i << "] = ";
-//		std::cin >> cnt;
-//		arr[i] = cnt;
-//	}
-//	std::cout << "Введенный массив: ";
-//	for (int j = 0; j < size; ++j)
-//	{
-//		std::cout << arr[j] << ' ';
-//	}
-//	delete[] arr;
-//	return EXIT_SUCCESS;
-//}
+#include <iostream>
+
+int main()
+{
+	int size{}, cnt{};
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+	int* arr = new int[size]();
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << "arr[" << i << "] = ";
+		std::cin >> cnt;
+		arr[i] = cnt;
+	}
+	std::cout << "Введенный массив: ";
+	for (int j = 0; j < size; ++j)
+	{
+		std::cout << arr[j] << ' ';
+	}
+	delete[] arr;
+	return EXIT_SUCCESS;
+
+}
